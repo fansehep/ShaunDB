61f5dad536f3dcc787a5a593fee370ec4620c197
fan <yfan3763@gmail.com>
2022-09-02 21:42:55 +0800

    src/net: writing ...
    Signed-off-by: fan <yfan3763@gmail.com>

--- src/net/Poller.hpp
+++ src/net/Poller.hpp
@@ -1,16 +1,20 @@
 #ifndef SRC_BASE_NET_POLLER_H_
 #define SRC_BASE_NET_POLLER_H_
 
-#include "src/base/NonCopyable.hpp"
-#include "src/base/log/Logging.hpp"
-#include "src/base/TimeStamp.hpp"
-#include <vector>
 #include <map>
+#include <vector>
+
+#include "src/base/NonCopyable.hpp"
+#include "src/base/TimeStamp.hpp"
+#include "src/base/log/Logging.hpp"
+#include <errno.h>
+
+using fver::base::TimeStamp;
 
 namespace fver::net {
 
 /*
-1. 
+1.
    struct epoll_event {
     uint32_t events;
     struct epoll_data {
@@ -37,24 +41,68 @@ namespace fver::net {
 class EventLoop;
 class Channel;
 
-
 template <typename T>
 class Poller : public fver::base::NonCopyable {
  public:
-  Poller(EventLoop* loop);
-  ~Poller();
-  template <typename T>
-  fver::base::TimeStamp UpdateChannel(std::vector<Channel>& channums, int timeoutms);
-  template <typename T>
-  void RemoveChannel(Channel* channel);
-  template <typename T>
-  void HasChannel(Channel* channel);
- private:
-  std::map<int, Channel*> ChannelMap_;
+  Poller() = default;
+  ~Poller() = default;
+
+  fver::base::TimeStamp UpdateChannelLists(std::vector<Channel*>* channums,
+                                      int timeoutms) {
+    return static_cast<T*>(this)->UpdateChanelLists(channums, timeoutms);
+  }
+
+  void UpdateChannel(Channel* channel) {
+    return static_cast<T*>(this)->UpdateChannel(channel);
+  }
+
+  void RemoveChannel(Channel* channel) {
+    return static_cast<T*>(this)->RemoveChannel(channel);
+  }
+
+  void HasChannel(Channel* channel) {
+    return static_cast<T*>(this)->HasChannel(channel);
+  }
 };
 
-class Poll : public Poller<> {
+class Poll : public Poller<Poll> {
+ public:
+  Poll(EventLoop* loop) : loop_(loop) {}
 
+  TimeStamp UpdateChannelLists(std::vector<Channel*>* channums,
+                                      int timeoutms) {
+    int events_n = ::poll(pollnums_.begin(), pollnums_.size(), timeoutms);
+    auto savederror = errno;
+    TimeStamp tnow(TimeStamp::Now());
+    if (events_n > 0) {
+      LOG_TRACE("Poller {} events_n react", events_n);
+      for (auto pfd = pollnums_.begin(); pfd != pollnums_.end() && events_n > 0; ++pfd) {
+        if (pfd->revents > 0) {
+          --events_n;
+          auto reventschaniter = channelMap_.find(pfd->fd);
+          auto channptr = reventschaniter.second;
+          channptr->set_revents(pfd->revents);
+          channums->push_back(channptr);
+        }
+      }
+    } else if (events_n == 0) {
+      LOG_INFO("Poller No events react");
+    } else {
+      if (savederror != EINTR) {
+        LOG_ERROR("Poller Error!");
+      }
+    }
+    return tnow;
+  }
+
+  void RemoveChannel(Channel* channel) {}
+
+  void HasChannel(Channel* channel) {}
+
+ private:
+  std::map<int, Channel*> channelMap_;
+  EventLoop* loop_;
+  std::vector<struct pollfd> pollnums_;
 }
 
 }  // namespace fver::net
