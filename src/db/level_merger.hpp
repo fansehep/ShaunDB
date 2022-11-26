4b8dea74277cc7ec318d1cfbc8abade4f796c253
fan <yfan3763@gmail.com>
2022-11-26 21:01:41 +0800

    add doc and finish level merge
    Signed-off-by: fan <yfan3763@gmail.com>

--- src/db/level_merger.hpp
+++ src/db/level_merger.hpp
@@ -1,23 +1,21 @@
 #ifndef SRC_DB_LEVEL_MERGER_H_
 #define SRC_DB_LEVEL_MERGER_H_
 
+#include "src/db/memtable_view.hpp"
 
 namespace fver {
 
 namespace db {
 
-
-void MergeLevel();
+//
+bool MergeLevel(const MemTable_view& leftMview,
+                const MemTable_view& rightMview,
+                MemTable_view* result);
 
 
 
 }
 
-
-
-
-}
-
-
+}  // namespace fver
 
 #endif
