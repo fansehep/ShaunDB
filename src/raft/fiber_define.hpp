08cf907e1bb60f5b75b29f1abb2ca76cf5ff0327
fan <yfan3763@gmail.com>
2023-04-02 14:12:04 +0800

    [fix] fix fiber print
    Signed-off-by: fan <yfan3763@gmail.com>

--- src/raft/fiber_define.hpp
+++ src/raft/fiber_define.hpp
@@ -1,31 +1,55 @@
 #ifndef SRC_RAFT_FIBER_DEFINE_H_
 #define SRC_RAFT_FIBER_DEFINE_H_
 
-#include <boost/fiber/algo/work_stealing.hpp>
-#include <boost/fiber/all.hpp>
-#include <boost/fiber/buffered_channel.hpp>
-#include <boost/fiber/condition_variable.hpp>
-#include <boost/fiber/mutex.hpp>
-#include <boost/fiber/timed_mutex.hpp>
-#include <boost/fiber/operations.hpp>
-
-
+#include <thread>
+#include "boost/fiber/all.hpp"
+#include "fmt/format.h"
 
 /**
  * @_@ i am also a goer?
  */
 using go = boost::fibers::fiber;
 
+// for shaundb::log print
+template <>
+struct fmt::formatter<boost::fibers::fiber::id> {
+  constexpr auto parse(const format_parse_context& context) {
+    return context.begin();
+  }
+  template <typename Context>
+  constexpr auto format([[maybe_unused]] const boost::fibers::fiber::id& id,
+                        Context& cont) const {
+    return fmt::format_to(cont.out(), "{}",
+                          (void*)boost::fibers::context::active());
+  }
+};
+
+template <>
+struct fmt::formatter<std::thread::id> {
+  constexpr auto parse(const format_parse_context& context) {
+    return context.begin();
+  }
+  template <typename Context>
+  constexpr auto format(const std::thread::id& id, Context& cont) const {
+    return fmt::format_to(cont.out(), "{}", *((uint64_t*)&id));
+  }
+};
+
 
 namespace fiber {
-using fmutex = boost::fibers::mutex;
-using ftimed_mutex = boost::fibers::timed_mutex;
-using fcondtion = boost::fibers::condition_variable;
-using fcondtion_any = boost::fibers::condition_variable_any;
-
+  using fmutex = boost::fibers::mutex;
+  using ftimed_mutex = boost::fibers::timed_mutex;
+  using fcond = boost::fibers::condition_variable;
+  using fcond_any = boost::fibers::condition_variable_any;
 }
 
+#define fiber_yield boost::fiber::yield();
 
 
+/**
+ * @brief thread_n: boost::fiber M:N 所占用 N 线程的个数
+ * @brief
+ */
+static void init_fiber(const uint32_t thread_n) {}
 
 #endif
